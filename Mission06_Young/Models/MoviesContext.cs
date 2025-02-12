15795431afde61826d00fb38224e90ff138f7467
annay <akyoung@byu.edu>
2025-02-12 14:06:18 -0700

    Added database connections

---
+++ Mission06_Young/Models/MoviesContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Young.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext(DbContextOptions<MoviesContext> options) : base (options) { }
+
+        public DbSet<Form> Movies { get; set; }
+    }
+}
